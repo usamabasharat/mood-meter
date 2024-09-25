2dd748776e56336d80b68a6f6d350d86cdb89d50
Usama Basharat <dev.usamabasharat@gmail.com>
2024-09-25 06:13:10 -0700

    Implemented files structure

---
+++ app/dashboard/page.js
@@ -0,0 +1,26 @@
+import Dashboard from "@/components/Dashboard";
+import Login from "@/components/Login";
+import Main from "@/components/Main";
+
+export const metadata = {
+  title: "Mood Meter - Dashboard",
+};
+
+export default function DashboardPage(){
+  const isAuthenticated = false;
+
+  let children = (
+    <Login />
+  )
+
+  if (isAuthenticated) {
+    children = (
+      <Dashboard />
+    )
+  }
+  return (
+    <Main>
+      {children}
+    </Main>
+  )
+}
