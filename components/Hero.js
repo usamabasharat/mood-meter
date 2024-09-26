1234c4bbce3e9c59d62978b70d8944f07b5cb932
Usama Basharat <dev.usamabasharat@gmail.com>
2024-09-26 03:36:50 -0700

    Implemented calendar component

--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,7 @@
 import { Fugaz_One } from "next/font/google";
 import React from 'react'
 import CallToAction from "./CallToAction";
+import Calendar from "./Calendar";
 
 const fugaz = Fugaz_One({ subsets: ["latin"], weight: ['400'] });
 
@@ -10,6 +11,7 @@ export default function Hero() {
           <h1 className={'text-5xl sm:text-6xl md:text-7xl text-center ' + fugaz.className}><span className='textGradient'>Mood Meter</span> helps you track your <span className='textGradient'>daily</span> mood!</h1>
           <p className='text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w-[600px]'>Create your mood record and see how you feel on <span className='font-semibold'>every day of every year.</span></p>
           <CallToAction />
+          <Calendar demo />
       </div>
   )
 }
